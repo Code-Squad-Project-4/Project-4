31d9768ff0b021dfff4d95378637c48cfc182b42
Shehadeh-Al-Momani <shehadeh.almomani@gmail.com>
2020-11-12 19:06:07 +0200

    View a specific product's details

--- back-end/controllers/main-controller.js
+++ back-end/controllers/main-controller.js
@@ -12,7 +12,19 @@ const allProducts = async (req, res) => {
    }
 };
 
+const productDetails = async (req, res) => {
+    try {
+     let sql = `SELECT * FROM product WHERE name = ${req.params.id}`;
+     let query = db.query(sql, (err, results) => {
+         if (err) throw err;
+         res.json( results);
+     });
+    } catch (error) {
+        throw error;
+    }
+ };
 
 module.exports = {
     allProducts,
+    productDetails,
 }
--- back-end/routes/main-route.js
+++ back-end/routes/main-route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mainRouter = express.Router();
-const {allProducts} = require('../controllers/main-controller')
+const {allProducts,productDetails} = require('../controllers/main-controller')
 
 mainRouter.get("/", (req, res) => {
   res.json("HELLO WORLD");
@@ -8,5 +8,8 @@ mainRouter.get("/", (req, res) => {
 
 // View all products
 mainRouter.get("/allProducts", allProducts);
+
+// View a specific product's details
+mainRouter.get("/product/:id", productDetails);
 
 module.exports = mainRouter;
