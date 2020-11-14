9af7cf2eb09c25c5f494665820663da165f986be
unknown <omar.q.alkhatib@gmail.comgit config --global user.name Omar-Alkhatib git config --global user.email "omar.q.alkhatib@gmail.comgit config --global user.name "Omar-Alkhatib>
2020-11-14 15:00:35 +0200

    separeted the controller and the route for seller-account-page

--- back-end/routes/main-route.js
+++ back-end/routes/main-route.js
@@ -1,17 +1,5 @@
 const express = require("express");
 const mainRouter = express.Router();
-const {getUserURL,
-  getCurrentMonthlySales,
-  totalNumberOfItemsAvailable,
-  totalNumberOfItemsSold,
-  sellerRating} = require("../controllers/main-controller");
-
-   mainRouter.get("/seller_URL", getUserURL)
-   mainRouter.get("/monthly_sales", getCurrentMonthlySales)
-   mainRouter.get("items_available", totalNumberOfItemsAvailable)
-   mainRouter.get("items_sold", totalNumberOfItemsSold)
-   mainRouter.get("seller_rating", sellerRating)
-
 
 // mainRouter.get("/seller_URL", (req, res) => {
 //   res.json("HELLO WORLD");
