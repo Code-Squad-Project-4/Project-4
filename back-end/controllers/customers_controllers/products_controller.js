d46c38c13de0eb4ad17f5bb0f18982c91ff2c4fd
Shehadeh-Al-Momani <shehadeh.almomani@gmail.com>
2020-11-15 11:23:41 +0200

    fix require db

--- back-end/controllers/customers_controllers/products_controller.js
+++ back-end/controllers/customers_controllers/products_controller.js
@@ -1,4 +1,4 @@
-const db = require('../db');
+const db = require('../../db');
 
 const viewAllProducts = async (req, res) => {
     try {
--- back-end/controllers/customers_controllers/rating.controller.js
+++ back-end/controllers/customers_controllers/rating.controller.js
@@ -1,4 +1,4 @@
-const db = require('../db');
+const db = require('../../db');
 
 const customerRating = async (req, res) => {
     try {
