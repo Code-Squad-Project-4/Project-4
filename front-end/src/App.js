04d73273fdd5999a13b7a1d03eb9e961770f1587
Shehadeh-Al-Momani <shehadeh.almomani@gmail.com>
2020-11-18 13:39:40 +0200

    create about

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,11 +3,21 @@ import './App.css';
 import axios from 'axios';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import NavBar from "./components/NavBar";
+import About from "./components/About";
 
 const App = () => {
+  const [info, setInfo] = useState([
+    { name: 'Alaa Khalila', position: 'Full-Stack Dev', phone: '0795846987', email: 'Alaa@gmail.com' },
+    { name: 'Fadi Al-fuqaha', position: 'Full-Stack Dev', phone: '0785846987', email: 'Fadi@gmail.co' },
+    { name: 'Omar Alkhatib', position: 'Full-Stack Dev', phone: '0775846987', email: 'Omar@gmail.com' },
+    { name: 'Shehadeh Almomani', position: 'Full-Stack Dev', phone: '0795846988', email: 'Shehadeh@gmail.com' }
+  ]);
+  
   return (
     <Router>
       <NavBar />
+
+      <Route path="/about" render={(props) => <About {...props} {...info} />} />
 
     </Router>
   );
