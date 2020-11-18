8fabc8e71e8e169f6fe7977b4df3a70d961df094
Shehadeh-Al-Momani <shehadeh.almomani@gmail.com>
2020-11-18 14:19:57 +0200

    create categories

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,14 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import TodoList from "./TodoList";
 import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import CategoriesList from "./CategoriesList"
  
  const Home = (props) => {
+     const [categories,setCategories] = useState([]);
+     useEffect(()=>{getCategories()},[])
+
+     const getCategories = async () => {
+        try {
+          const res = await axios.get('http://localhost:5000/categories')
+          console.log('getCategories :', res.data)
+          await setCategories(res.data);
+        }
+        catch (err) {
+          console.log('ERR: ', err);
+        };
+      }
+
     return (
         <div className="home">
             <Route exact path="/">
                 <div className="firstHome">
                     <div className="categories">
-                    b
+                    <CategoriesList categories={categories}/>
                     </div>
                     <div className="Featured">
                         <div className="mainFeatured"></div>
